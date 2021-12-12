321ccd3278e49d424ab26cf7efdf24a731638319
Michael Vlamis <michael.vlamis@icloud.com>
2021-12-11 22:54:16 -0500

    create gui

--- tts.py
+++ tts.py
@@ -1,9 +1,35 @@
+import tkinter
 import pyttsx3
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-for voice in voices:
-   print(voice.id)
-   engine.setProperty('voice', voice.id)
-   engine.say('The quick brown fox jumped over the lazy dog.')
-   engine.runAndWait()
-engine.runAndWait()
+from tkinter import *
+ 
+def speak():
+    engine = pyttsx3.init()
+    engine.say(testEntry.get())
+    engine.runAndWait()
+
+# Create a GUI window
+root = Tk()
+root.geometry("900x600")
+root.title("Test")
+
+frame = Frame(root)
+frame.pack()
+
+
+testEntry = Entry(frame, width = 20)
+testEntry.insert(0,'Text to speak')
+testEntry.pack(padx = 5, pady = 5)
+
+Button = Button(frame, text = "Submit", command = speak)
+Button.pack(padx = 5, pady = 5)
+
+root.mainloop()
+
+
+# engine = pyttsx3.init()
+# voices = engine.getProperty('voices')
+# engine.say('The quick brown fox jumped over the lazy dog.')
+# engine.runAndWait()
+
+# engine.runAndWait()
+
